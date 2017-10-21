d4a9f535edd9ebc5aa7932bd53d84fb27f1535b8
m5ba <mbalonkin@gmail.com>
2017-10-21 10:44:26 -0500

    init pass

---
+++ Point.java
@@ -0,0 +1,32 @@
+import java.util;
+
+public class Point implements Comparable<Point> {
+  public Point(int x, int y) {
+
+  }
+
+  public void draw() {
+
+  }
+
+  public void drawTo(Point that) {
+
+  }
+
+  public String toString() {
+    return "";
+  }
+
+  public int compareTo(Point that) {
+    return 0;
+  }
+
+  public double slopeTo(Point that) {
+    return 0.0f;
+   
+  }
+
+  public Comparator<Point> slopeOrder() {
+    return 0.0f;
+  }
+}
